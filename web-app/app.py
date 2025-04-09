2bc31181ed229107c73ef35f17165cf3167e28c6
AndrewJung03 <andrewjung3245@gmail.com>
2025-04-09 13:58:05 -0400

    fix for lint error 3

--- web-app/app.py
+++ web-app/app.py
@@ -1,9 +1,11 @@
 """
 flask app
 """
+
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
@@ -11,6 +13,7 @@ def home():
     route handler for the home page.
     """
     return "Hello"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
